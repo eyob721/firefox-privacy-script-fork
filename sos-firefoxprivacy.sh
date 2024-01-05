c9c16126b07afedcc6eac78a14442c514f46f0af
eyob721 <eyobmelkamu721@gmail.com>
2024-01-05 19:24:24 +0300

    fix: adjust script to use firefox-developer-edition

--- sos-firefoxprivacy.sh
+++ sos-firefoxprivacy.sh
@@ -2,26 +2,29 @@
 
 # Function to display messages in red
 RED() {
-    echo -e "\e[91m$1\e[0m"
+	echo -e "\e[91m$1\e[0m"
 }
 
 # Testing if root...
 if [ $UID -ne 0 ]; then
-    RED "You must run this script as root!" && echo
-    exit
+	RED "You must run this script as root!" && echo
+	exit
 fi
 
+# Edit this to the version of firefox you are using, default is "firefox"
+firefox="firefox-developer-edition" # I am using the developer edition
+
 # Define the directories
-preferences_dir="/lib/firefox/browser/defaults/preferences/"
-distribution_dir="/lib/firefox/distribution/"
-extensions_dir="/lib/firefox/distribution/extensions/"
+preferences_dir="/lib/${firefox}/browser/defaults/preferences/"
+distribution_dir="/lib/${firefox}/distribution/"
+extensions_dir="/lib/${firefox}/distribution/extensions/"
 
 # Create directories if they don't exist
-mkdir -p "$preferences_dir"
-mkdir -p "$extensions_dir"
-mkdir -p "$distribution_dir"
+mkdir -p "${preferences_dir}"
+mkdir -p "${extensions_dir}"
+mkdir -p "${distribution_dir}"
 
 # Copy files to the directories
-cp ./Files/browser/defaults/preferences/* "$preferences_dir"
-cp ./Files/browser/distribution/* "$distribution_dir"
-cp ./Files/distribution/extensions/* "$extensions_dir"
+cp ./Files/browser/defaults/preferences/* "${preferences_dir}"
+cp ./Files/browser/distribution/* "${distribution_dir}"
+cp ./Files/distribution/extensions/* "${extensions_dir}"
